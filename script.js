722cdc94d16b00aa873c1abe02b257e475bb15a5
Ahmad <ahmad@ahmd-asdy.local>
2022-10-08 19:16:19 +0330

    fixing minor issues

--- script.js
+++ script.js
@@ -1,50 +1,26 @@
 // function for adding the value of pressed button to the display
 
-const display = document.getElementById("equation");
+let display = document.getElementById("equation");
 
-function inputText(x, y = 1) {
-	let d = display.innerText;
-	let text = "";
-	if (d[0] == 0 || d[0] == "%") {
-		display.innerText = "";
-	}
-	if (
-		(y == "a") &
-		(d[0] == "/" ||
-			d[0] == "x" ||
-			d[0] == "." ||
-			d[0] == "-" ||
-			d[0] == "+" ||
-			d[0] == "%" ||
-			d[0] == 0)
-	) {
-		x = 0;
-	}
-	if (y != 1) {
-		if (
-			d[d.length - 1] == "/" ||
-			d[d.length - 1] == "x" ||
-			d[d.length - 1] == "." ||
-			d[d.length - 1] == "-" ||
-			d[d.length - 1] == "+"
-		) {
-			for (let i = 0; i < d.length - 1; i++) {
-				text += d[i];
-			}
-			d = text;
-			display.innerText = d + x;
-		} else {
-			display.innerText += x;
-		}
-	} else {
-		display.innerText += x;
-	}
+function inputText(x) {
+	display.innerText += x;
 }
+
 // console.log(display.innerText);
 
 function all_clear() {
 	display.innerText = "0";
 }
+
+function deletText() {
+	let d = display.innerText;
+	let text = "";
+	for (let i = 0; i < d.length - 1; i++) {
+		text += d[i];
+	}
+	display.innerText = text;
+}
+
 function calculator(event) {
 	let vari = event.key;
 	console.log(vari);
@@ -59,11 +35,7 @@ function calculator(event) {
 		vari == 7 ||
 		vari == 8 ||
 		vari == 9 ||
-		vari == 0
-	) {
-		inputText(vari);
-	}
-	if (
+		vari == 0 ||
 		vari == "+" ||
 		vari == "-" ||
 		vari == "x" ||
@@ -71,12 +43,16 @@ function calculator(event) {
 		vari == "%" ||
 		vari == "."
 	) {
-		inputText(vari, "a");
+		inputText(vari);
 	}
+
 	if (vari == "c") {
 		all_clear();
 	}
 	if (vari == "*") {
-		inputText("x", " a");
+		inputText("x");
+	}
+	if (vari == "Backspace") {
+		deletText();
 	}
 }
